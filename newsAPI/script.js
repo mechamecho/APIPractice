261e50ba4d514ec23f74f0f0c893c3d732fa94e6
Nafissa Hassan <nafissa_92@hotmail.com>
2017-07-27 02:42:19 +0000

    walkthrough

--- newsAPI/script.js
+++ newsAPI/script.js
@@ -17,16 +17,48 @@ $(document).ready(function(){
             // console.log(response.sources[2]);
             var sources=response.sources;
             // console.log(sources[2]);
-            var html="<select class='form-control' id='source'>";
+            var html="<select class='form-control' id='sourceOption'>";
             $.each(sources, function(index,source){
                 // console.log(source);
-                html += "<option value='id?'>"+source.name+"</option>";
+                //will not be a problem with scope, because this will be aquired when the form is submitted, which 
+                //automatically make the second ajax call
+                html += "<option value='"+source.id+"'>"+source.name+"</option>";
             })
             html+= "</select>";
-            console.log(html);
+            // console.log(html);
             $(".form-group").html(html);
         }
     })
     //submit button
     //$('select').val()
+    //$("./#/name") targetting in jquery
+    $("#source").submit(function(event){
+        //to prevent the page from reloading on submission, which it usually does, native javascript api method
+        event.preventDefault();
+        var id = $("#sourceOption").val();
+        var url=" https://newsapi.org/v1/articles";
+        var data={apiKey:"d588f533b2f04010b0fc0b7666e9da7b", source:id};
+        $.ajax({
+            url:url,
+            data:data,
+            type:"GET",
+            //you only name a function if you are calling it more than once, otherwise you will be wasting memory
+            //it will refresh the articles because the content of the html variable will change depending on the new 
+            //response it gets from the api when the form is re-submitted
+            success:function(response){
+                // console.log(response);
+                var articles=response.articles;
+                var html="<ul class='list-group'>";
+                //if we only put article, then article will be the index number, the first parameter will always be the index
+                //regardless of what you name it
+                $.each(articles, function(index, article){
+                    html+="<li class='list-group-item'>"+article.title+"</li>"
+                })
+                html+="</ul>";
+                $("#articles").html(html);
+                
+            }
+        })
+        
+    })
 });
